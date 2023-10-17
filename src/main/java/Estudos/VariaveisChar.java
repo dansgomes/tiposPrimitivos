5da9cbf0576e585ea72f38faf103adf93d89b394
Daniel Gomes <danielsgomes99@gmail.com>
2023-10-17 18:15:28 -0300

    Estudos contendo variáveis primitivas e suas curiosidades

---
+++ src/main/java/Estudos/VariaveisChar.java
@@ -0,0 +1,16 @@
+package Estudos;
+
+public class VariaveisChar {
+    public static void main (String[] args){
+
+        //char o = 'o';
+
+        //char i = 'i';
+
+        char o = 111;
+        char i = 105;
+        char interrogacao = 0X00E1; //valor 'á' e não '?'
+
+        System.out.println("" + o+i+interrogacao);
+    }
+}
---
+++ src/main/java/Estudos/VariaveisFlutuantes.java
@@ -0,0 +1,14 @@
+package Estudos;
+
+public class VariaveisFlutuantes {
+
+    public static void main(String[] args) {
+
+        double valorPassagem = 2.90;
+
+        float valorTomate = 3.95f;
+
+        System.out.println("valor da passagem =" + valorPassagem);
+        System.out.println("valor da tomate =" + valorTomate);
+    }
+}
