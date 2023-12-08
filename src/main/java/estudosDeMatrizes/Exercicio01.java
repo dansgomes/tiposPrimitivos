868b0c1dda76e4d7e6cefd09aaa70259e66a092f
Daniel Gomes <danielsgomes99@gmail.com>
2023-12-08 17:33:30 -0300

    Estudos contendo variáveis e a adição de Vetores, Arrays e Matrizes.

--- src/main/java/estudosDeMatrizes/Exercicio01.java
+++ src/main/java/estudosDeMatrizes/Exercicio01.java
@@ -4,17 +4,39 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Exercicio01 {
-    public static void main (String [] args){
+    public static void main (String [] args) {
 
-    int [] [] numerosAleatorios = new int[4][4];
+        int[][] numerosAleatorios = new int[4][4];
 
         Random numeroRandom = new Random();
 
-        for (int i=0; i<numerosAleatorios.length; i++){
-            for (int j=0; j<numerosAleatorios[i].length; j++){
-                numerosAleatorios [i] [j] = numeroRandom.nextInt();
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                numerosAleatorios[i][j] = numeroRandom.nextInt(100);
             }
         }
 
-    }
-}
+        int maior = 0;
+        int linha = 0;
+        int col = 0;
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                if (numerosAleatorios[i][j] > maior) {
+                    maior = numerosAleatorios[i][j];
+                    linha = i;
+                    col = j;
+                }
+            }
+        }
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                System.out.println(numerosAleatorios[i][j] + "");
+            }
+            System.out.println();
+        }
+                System.out.println("Maior valor = " + maior);
+                System.out.println("Linha = " + linha);
+                System.out.println("Coluna = " + col);
+
+            }
+        }
