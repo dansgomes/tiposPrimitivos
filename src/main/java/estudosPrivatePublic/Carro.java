4550d3c8bd9212e9f0377540da2d5c1a8098983a
Daniel Gomes <danielsgomes99@gmail.com>
2024-01-17 18:39:31 -0300

    estudos de set() e get() e encapsulamento afim de expandir o conteúdo sobre modo private, default e public

---
+++ src/main/java/estudosPrivatePublic/Carro.java
@@ -0,0 +1,37 @@
+package estudosPrivatePublic;
+
+ class Carro {
+    private String marca;
+    private String modelo;
+    private int numPassageiros;
+    private double capCombustivel;
+    private double consumoCombustivel;
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    void exibirAutonomia(){
+
+        System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + "km");
+    }
+
+    public double obterAutonomia(){
+
+        System.out.println("Método obterAutonomia foi chamado. ");
+
+        return capCombustivel * consumoCombustivel;
+    }
+
+     private double divideKMPorConsumoCombustivel(double km){
+         return km/this.consumoCombustivel;
+     }
+
+    public double calcularCombustivel(double km){
+
+        return this.divideKMPorConsumoCombustivel(km);
+    }
+}
---
+++ src/main/java/estudosPrivatePublic/testeCarro.java
@@ -0,0 +1,18 @@
+package estudosPrivatePublic;
+
+import estudosConstrutores.CarroThis;
+import estudosMetodosEClasses.Carro_7;
+
+public class testeCarro {
+
+    public static void main(String [] args){
+
+        Carro carro = new Carro();
+        carro.setMarca("Fiat");
+        System.out.println(carro.getMarca());
+
+
+
+
+    }
+}
