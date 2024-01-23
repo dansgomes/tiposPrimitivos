1769f76fa5a42644cd2e91740fd20b49ec8dcdfc
Daniel Gomes <danielsgomes99@gmail.com>
2024-01-23 17:38:47 -0300

    estudos de herança em java afim de aprender e fixar sobre classes que herdam e compartilham atributos de outras classes.

---
+++ src/main/java/estudosHeranca/Teste.java
@@ -0,0 +1,23 @@
+package estudosHeranca;
+
+public class Teste {
+    public static void main (String [] args){
+
+        /*Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Maria");
+        aluno.getNome();*/
+
+        //professor.setTelefoneCelular(telefoneCelular);
+
+        Pessoa aluno = new Aluno();
+        //aluno.get
+
+        Pessoa professor = new Professor();
+        //professor.get
+    }
+}
