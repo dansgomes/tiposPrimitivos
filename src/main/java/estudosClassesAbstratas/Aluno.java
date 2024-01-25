9872d885fbc9c0310c400c7f519fb1cb00efe724
Daniel Gomes <danielsgomes99@gmail.com>
2024-01-25 19:24:00 -0300

    estudos de herança em java afim de aprender e fixar conceitos sobre tipo de classe abstract e sua relação com classes filhas (que precisam ser instanciadas)

---
+++ src/main/java/estudosClassesAbstratas/Aluno.java
@@ -0,0 +1,55 @@
+package estudosClassesAbstratas;
+
+public  class Aluno extends Pessoa {
+
+
+    private String curso;
+    private double[] notas;
+
+    public void verificarAcesso(){
+
+        this.nomeVisibilidade = "seshe";
+        super.nomeVisibilidade = "seche";
+    }
+
+    public Aluno() {
+        super();
+    }
+
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+
+    public boolean verificarAprovado(){
+        return true;
+    }
+
+    public String obterEtiquetaEndereco (){
+
+        String s = "Endereço do Aluno: ";
+        s += super.getEndereco();
+
+        return s;
+    }
+
+    public void imprimirEtiquetaEndereco(){
+        System.out.println(this.obterEtiquetaEndereco());
+    }
+}
