60d4ec8c5b23a271ac1160964596ccf20c2882e4
Daniel Gomes <danielsgomes99@gmail.com>
2024-01-26 17:21:18 -0300

    estudos de herança em java afim de aprender e fixar conceitos sobre tipo finals e como eles podem ser usados para classes imutáveis, assim como aprendemos boas práticas sobre constantes.

---
+++ src/main/java/estudosFinals/Constantes.java
@@ -0,0 +1,9 @@
+package estudosFinals;
+
+public final class Constantes {
+
+    public static final String URL_BLOG = "http://daniel.com";
+    public static final String CURSO_COMPLETO = "http://loiane.training";
+
+    //private String urlBlog;
+}
---
+++ src/main/java/estudosFinals/Teste.java
@@ -0,0 +1,21 @@
+package estudosFinals;
+
+import estudosHeranca.Pessoa;
+
+public class Teste {
+    public static void main (String [] args){
+
+        Pessoa pessoa = new Pessoa();
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+
+        //exemplo02
+        System.out.println(Constantes.URL_BLOG);
+        //Constantes.URL_BLOG = "sdsdkhds";
+        System.out.println(Constantes.URL_BLOG);
+
+        int TOTAL = 1;
+        //total = total + 1;
+        System.out.println(TOTAL);
+    }
+}
