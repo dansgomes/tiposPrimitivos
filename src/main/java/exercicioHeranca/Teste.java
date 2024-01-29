d620eb7da53ac8675aa9d08d6c16d1ec6f6d9838
Daniel Gomes <danielsgomes99@gmail.com>
2024-01-29 20:58:19 -0300

    início do exercício prático de herança afim de treinar conceitos aprendidos nas aulas. O intuito é criar uma Conta Bancária utilizando tudo aprendido previamente.

---
+++ src/main/java/exercicioHeranca/Teste.java
@@ -0,0 +1,28 @@
+package exercicioHeranca;
+
+public class Teste {
+    public static void main (String [] args){
+
+        System.out.println("*** Teste Conta Bancária ***");
+
+        ContaBancaria contaSimples = new ContaBancaria();
+        contaSimples.setNomeCliente("Cliente Conta Simples");
+        contaSimples.setNumConta("11111");
+
+        contaSimples.depositar(100);
+
+        realizarSaque(contaSimples, 50);
+
+        realizarSaque(contaSimples, 70);
+
+        System.out.println(contaSimples);
+    }
+
+    private static void realizarSaque(ContaBancaria conta, double valor){
+        if (conta.sacar(valor)){
+            System.out.println("Saque efetuado com sucesso, novo saldo = " + conta.getSaldo());
+        } else {
+            System.out.println("Saldo insuficiente para saque de " + 50 + "; saldo de = " + conta.getSaldo());
+        }
+    }
+}
