922d6bc3f9c6958917e349ce8c6928bce41d6bbc
Daniel Gomes <danielsgomes99@gmail.com>
2024-02-07 20:59:00 -0300

    Continuidade do estudo de Exceptions no Java, introduzindo métodos para se fazer suas próprias exceptions e testa-las

---
+++ src/main/java/estudosExceptions/DivisaoNaoExata.java
@@ -0,0 +1,20 @@
+package estudosExceptions;
+
+public class DivisaoNaoExata extends Exception{
+
+    private int num;
+    private int dem;
+
+    public DivisaoNaoExata(int num, int dem) {
+        this.num = num;
+        this.dem = dem;
+    }
+
+    @Override
+    public String toString() {
+        return "Resultado de =" +
+                 num + "/" +
+                 dem + "não é um inteiro" +
+                '}';
+    }
+}
